0833360355428d265e160c494060b5a9fb002528
timothyAgevi <timothyagevi@gmail.com>
2022-03-13 16:58:06 +0300

    update mjs

--- index.mjs
+++ index.mjs
@@ -3,4 +3,15 @@ import * as backend from './build/index.main.mjs';
 
 (async ()=>{
     const stdlib =await loadstdlib();//env variables,local nodes
+    //accounts
+    const accAlice= await stdlib.newTestAccount(stdlib.parseCurrency(5));
+    const accBob= await stdlib.newTestAccount(stdlib.parseCurrency(10));
+    //contracts
+    const ctcAlice=await accAlice.deploy(backend);
+    const ctcBob=await accBob.attach(backend,ctcAlice);
+
+    await Promise.all([
+        
+    ])
+
 })();
